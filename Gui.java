2b0d60634b711ccca5006fcae76201aa0dec614c
Jenny Luc <jeluc@ucsd.edu>
2018-10-13 16:29:18 -0700

    added gui file

---
+++ Gui.java
@@ -0,0 +1,11 @@
+import javax.swing;
+import java.awt;
+import java.awt.event.*;
+
+public class GuiFile extends JFrame {
+
+    public static void main (String[] args)
+    {
+        MyGUI theGUI = new MyGUI();
+    }
+}
